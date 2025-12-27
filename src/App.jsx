f46b2c292d7e202866f50080494147de94ff3021
Tharaka-Maheshwara <159180873+Tharaka-Maheshwara@users.noreply.github.com>
2025-12-27 22:15:43 +0530

    Use State 😊

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import LoginPage from './pages/loginPage'
 import AdminPage from './pages/adminPage'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Testing from './pages/testing'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   
@@ -9,9 +11,11 @@ function App() {
   return (
     <div>
      <BrowserRouter>
+     <Toaster position='top-right'/>
           <Routes path= "/*">
             <Route path= "/admin/*" element={<AdminPage/>}/>
             <Route path = "/login" element={<LoginPage/>}/>
+             <Route path = "/testing" element={<Testing/>}/>
             <Route path='/' element={<h1>Home</h1>}/>
             <Route path='/*'  element={<h1>404 Not Found</h1>}/>
             </Routes>     
